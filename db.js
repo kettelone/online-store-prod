cbb6719e90f4c0a4d22b80b941a9f470ce7e2e10
Bohdan Krevsun <bkrevsun@gmail.com>
2022-06-04 16:13:51 +0400

    fourth-commit

--- db.js
+++ db.js
@@ -18,20 +18,3 @@ module.exports = new Sequelize({
     },
   },
 })
-
-// new Sequelize(
-//   process.env.DB_NAME, // Название БД
-//   process.env.DB_USER, // Пользователь
-//   process.env.DB_PASSWORD, // ПАРОЛЬ
-//   {
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     ssl: true,
-//     dialectOptions: {
-//       ssl: true,
-//       native: true,
-//     },
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//   }
-// )
